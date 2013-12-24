a9a2d985f02eb6adf2ff3f4c0394464c7d9d8852
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2013-12-25 00:53:06 +0100

    load stylesheets and javascript from cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+// require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require underscore
+// require turbolinks
+// require underscore
 //= require gmaps/google
 //= require_tree .
